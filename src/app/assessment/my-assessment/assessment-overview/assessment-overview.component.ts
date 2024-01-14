d3803661bc68c9c5aee83b1a4d739e79e21b4f5d
Dhawan Kumar <dhawankumar484@gmail.com>
2024-01-15 01:48:57 +0530

    updated mobile design

--- src/app/assessment/my-assessment/assessment-overview/assessment-overview.component.ts
+++ src/app/assessment/my-assessment/assessment-overview/assessment-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { IconsService } from 'src/app/services/icons.service';
 
 @Component({
@@ -7,16 +7,38 @@ import { IconsService } from 'src/app/services/icons.service';
   styleUrls: ['./assessment-overview.component.scss']
 })
 
-export class AssessmentOverviewComponent {
+export class AssessmentOverviewComponent implements OnInit{
 
-  totalAssessment:number = 34;
-  totalCandidates:number = 11000;
-  whoAttempted:number = 114;
-  email:number = 11000;
-  socialShare:number = 145;
-  uniqueLink:number = 145;
-  totalPurpose = 11;
+  totalAssessment:String = "34";
+  totalCandidates:String = "11145";
+  whoAttempted:String = "114";
+  email:String = "11000";
+  socialShare:String = "145";
+  uniqueLink:String = "145";
+  totalPurpose:String = "11";
+
 
   constructor(public iconService:IconsService){}
 
-}
+  ngOnInit(): void {
+      this.totalAssessment = String(this.numberFormatter(Number(this.totalAssessment)));
+      this.totalCandidates = String(this.numberFormatter(Number(this.totalCandidates)));
+      this.whoAttempted = String(this.numberFormatter(Number(this.whoAttempted)));
+      this.email = String(this.numberFormatter(Number(this.email)));
+      this.socialShare = String(this.numberFormatter(Number(this.socialShare)));
+      this.uniqueLink = String(this.numberFormatter(Number(this.uniqueLink)));
+      this.totalPurpose = String(this.numberFormatter(Number(this.totalPurpose)));
+
+  }
+
+  numberFormatter(value:number) {
+    if(value>999999){
+      return ((value)/1000000).toFixed(1) + 'M';
+    }
+    else if(value>19999){
+      return ((value)/1000).toFixed(1) + 'K';
+    }
+    else
+      return value;
+    }  
+  }
